b1303600c46e71904e934b0706326bae91a66b2d
MrDarkG <vako.batiashvili1998@gmail.com>
2024-08-17 14:39:00 +0400

    z

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,24 +1,12 @@
 <?php
 
-class DepartmentController extends Controller
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
 {
-    public function list(DepartmentListRequest $request, DepartmentService $service): JsonResponse
-    {
-        $source = env('source');
-        $list = $service->list($request, $source);
-        $resource = new DepartmentListResource($list);
-
-        return $resource->response()->setStatusCode(Response::HTTP_OK);
-    }
-
-    public function getFullNameAttribute()
-    {
-        return "{$this->first_name} {$this->last_name}";
-    }
-
-
-    public function setFirstNameAttribute($value)
-    {
-        $this->attributes['first_name'] = strtolower($value);
-    }
+    use AuthorizesRequests, ValidatesRequests;
 }
